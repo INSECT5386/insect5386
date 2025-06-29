93529b6ee27d8934147eadd7a886b0a3045a366b
INSECT5386 <shinnara.yuchan@gmail.com>
2025-06-29 16:32:29 +0900

    Update Model.py

--- Model.py
+++ Model.py
@@ -6,6 +6,20 @@ from tensorflow.keras import layers
 from tensorflow.keras.initializers import RandomNormal
 import sentencepiece as spm  
 import requests
+from tensorflow.keras.initializers import RandomNormal
+
+import tensorflow as tf
+from tensorflow.keras import layers, Model, Input
+
+import tensorflow as tf
+from tensorflow.keras import layers, Model, Input
+from tensorflow.keras.initializers import RandomNormal
+
+
+import tensorflow as tf
+from tensorflow.keras import layers, Model, Input
+from tensorflow.keras.initializers import RandomNormal
+from tensorflow.keras.layers import Dropout
 
 # ⬇️ 파일 다운로드 함수
 def download_file(url, save_path):
