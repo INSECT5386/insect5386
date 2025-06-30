8305c9812c097e9b3caf356299a6a82b853d32cc
INSECT5386 <shinnara.yuchan@gmail.com>
2025-06-30 18:52:37 +0900

    Update Model.py

--- Model.py
+++ Model.py
@@ -227,16 +227,16 @@ dropout_rate = 0.1
 encoder_input = Input(shape=(max_enc_len,), name='encoder_input')
 x_emb = layers.Embedding(input_dim=vocab_size, output_dim=d_model)(encoder_input)
 x_pos = LearnablePositionalEmbedding(max_enc_len, d_model)(x_emb)
-x_pos = SelectiveFilter(d_model)(x_pos)
-context_vector = SeProdBlock(d_model, dropout_rate=dropout_rate)(x_pos, x_pos, training=True)
+x_pos = GLALayer(d_model)(x_pos, x_pos)
+context_vector = SeProdBlock(d_model, dropout_rate=dropout_rate)(x_pos, training=True)
 
 # 디코더 경로
 decoder_input = Input(shape=(max_dec_len,), name='decoder_input')
 y_emb = layers.Embedding(input_dim=vocab_size, output_dim=d_model)(decoder_input)
 y_pos = LearnablePositionalEmbedding(max_dec_len, d_model)(y_emb)
-decoder_output = SeProdBlock(d_model, dropout_rate=dropout_rate)(y_pos, y_pos, training=True)
-decoder_output = SelectiveFilter(d_model)(decoder_output)
-output = SeProdBlock(d_model, dropout_rate=dropout_rate)(decoder_output, context_vector)
+decoder_output = GLALayer(d_model, dropout_rate=dropout_rate)(y_pos, y_pos, training=True)
+decoder_output = GLALayer(d_model)(decoder_output, context_vector)
+output = SeProdBlock(d_model, dropout_rate=dropout_rate)(decoder_output)
 
 # 최종 출력
 logits = layers.Dense(vocab_size)(output)
