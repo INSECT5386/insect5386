608f0e2c4563131680bd90de2a5d40eda3c92654
Yuchan <shinnara.yuchan@gmail.com>
2025-11-14 16:39:16 +0900

    Update unigram.py

--- unigram.py
+++ unigram.py
@@ -8,7 +8,7 @@ def download_file(url, save_path):
     r = requests.get(url, stream=True)
     r.raise_for_status()
     with open(save_path, "wb") as f:
-        for chunk in r.iter_content(8192):
+        for chunk in r.iter_content(8192*4):
             f.write(chunk)
     print(f"✅ {save_path} 저장됨")
 
@@ -48,3 +48,4 @@ if __name__ == "__main__":
     train_bpe_tokenizer('dataset.txt', model_prefix="bpe", vocab_size=23400)
 
     print(f"✅ '{model_prefix}.model' / '{model_prefix}.vocab' 생성 완료!")
+
